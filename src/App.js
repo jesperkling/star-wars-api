31b31380d5293b436050c843cccf459932bddf8e
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-04-23 20:17:45 +0200

    Added a navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Navigation from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-		
+			<Navigation />
 		</div>
 	);
 }
