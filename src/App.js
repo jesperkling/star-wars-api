d79344be5e35e5c1303bf4dc01fa0821c7e0a831
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-04-24 11:51:41 +0200

    Changed name to Encyclopedia.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import Navigation from './components/Navigation';
-import 'bootstrap/dist/css/bootstrap.css'
-import './App.css';
+import Navigation from "./components/Navigation";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<Navigation />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navigation />
+
+      <h1>Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,30 @@
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { Link, NavLink } from 'react-router-dom'
-
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
-	return (
-		<Navbar bg='dark' variant='dark' expand='md'>
-			<Container>
-				<Navbar.Brand as={Link} to='/'>Star Wars Pedia</Navbar.Brand>
+  return (
+    <Navbar bg="dark" variant="dark" expand="md">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Star Wars Encyclopedia
+        </Navbar.Brand>
 
-				<Navbar.Toggle aria-controls='basic-navbar-nav'/>
-				<Navbar.Collapse id='basic-navbar-nav'>
-					<Nav className='ms-auto'>
-						<Nav.Link as={NavLink} end to='/people'>People</Nav.Link>
-						<Nav.Link as={NavLink} end to='/films'>Films</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} end to="/people">
+              People
+            </Nav.Link>
+            <Nav.Link as={NavLink} end to="/films">
+              Films
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-		</Navbar>
-	)
-}
-
-export default Navigation
+export default Navigation;
