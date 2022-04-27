23d9c5be07bc14d5e16b1d21efcb7abb07b07291
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-04-27 22:53:52 +0200

    Films page is now working and showing films.

--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -1,7 +1,53 @@
+import { useState, useEffect } from 'react'
+import { Button, Card, Container, Col, ListGroup, Row, ListGroupItem } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import CardHeader from 'react-bootstrap/esm/CardHeader'
+import swapi from '../services/swapi'
+
 const Films = () => {
+	const [films, setFilms] = useState("")
+
+	const getFilms = async () => {
+		const data = await swapi.getFilms()
+		setFilms(data)
+		console.log(data.results)
+	}
+
+	useEffect(() => {
+		getFilms()
+	}, [])
+	
 	return (
 		<>
-			<h1>Films</h1>
+			<Container>
+				<h1 className='text-center'>Films</h1>
+
+				<Row xs={1} md={2} lg={3}>
+					{films && films.results.map((films) => (
+						<Col>
+							<Card className='mb-4'>
+								<CardHeader>{films.title}</CardHeader>
+								<ListGroup>
+									<ListGroupItem>
+										Episode: {films.episode_id}
+									</ListGroupItem>
+									<ListGroupItem>
+										Release Date: {films.release_date}
+									</ListGroupItem>
+								</ListGroup>
+
+								<Card.Body>
+									<Button
+										className='btn'
+										as={Link}
+										to={`/films/${films.episode_id}`}
+									>Click here</Button>
+								</Card.Body>
+							</Card>
+						</Col>
+					))}
+				</Row>
+			</Container>
 		</>
 	)
 }
