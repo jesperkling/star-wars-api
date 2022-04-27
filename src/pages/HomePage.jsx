a5d3611b2c1c9b70c4b7698d2f71bd9d824120d0
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-04-27 19:42:23 +0200

    styled homepage and added a picture of Yoda.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
+import Image from 'react-bootstrap/Image'
+import Yoda from '../assets/images/yoda.jpg'
+
 const HomePage = () => {
     return (
-        <>
+        <div className='justify-content-center align-items-center text-center'>
             <h1> Welcome to my Star Wars Encyclopedia </h1>
-        </>
-    );
-};
 
-export default HomePage;
+            <Image src={Yoda} fluid></Image>
+        </div>
+    )
+}
+
+export default HomePage
